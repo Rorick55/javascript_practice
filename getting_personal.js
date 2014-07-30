e1ce69a85c7a593dcb5d1dc7e544bae106cabb28
TeamRory <rorycteam@gmail.com>
2014-07-30 11:20:06 -0400

    getting personal in js

---
+++ getting_personal.js
@@ -0,0 +1,9 @@
+var person = new Object();
+person.first_name = 'Rory';
+person.last_name = 'Team';
+person.full_name = person.first_name + ' ' + person.last_name;
+
+
+console.log(person.first_name);
+console.log(person.last_name);
+console.log(person.full_name);
