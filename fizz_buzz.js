335d4d96b0133415286aa434cc347791eae289de
TeamRory <rorycteam@gmail.com>
2014-07-30 11:27:23 -0400

    improved fizzbuzz

--- fizz_buzz.js
+++ fizz_buzz.js
@@ -1,11 +1,13 @@
 for(var i=1; i<101; i++) {
-  if (i % 3 === 0 && i % 5 === 0){
-    console.log('FizzBuzz');
-  } else if(i % 3 === 0) {
-    console.log('Fizz');
-  } else if(i % 5 === 0){
-    console.log('Buzz');
-  } else {
-    console.log(i);
+  var output = ''
+  if(i % 3 === 0) {
+    output = output + 'Fizz';
+  };
+  if(i % 5 === 0){
+    output = output + 'Buzz';
+  };
+  if(i % 3 !== 0 && i % 5 !== 0){
+    output = i
   }
+  console.log(output);
 }
