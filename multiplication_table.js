3f6911feb41fd2bdadcf0e85eba8d3dec65fe662
TeamRory <rorycteam@gmail.com>
2014-07-31 14:44:40 -0400

    fixed the look of the output

--- multiplication_table.js
+++ multiplication_table.js
@@ -1,8 +1,10 @@
 function multiplicationTable(row) {
   for (var i = 0; i <= row; i++){
+    var completeRow = [];
     for (var j = 0; j <= row; j++){
-     console.log(i * j);
+     completeRow.push(i * j);
     };
+    console.log(completeRow)
   };
 };
 multiplicationTable(12)
